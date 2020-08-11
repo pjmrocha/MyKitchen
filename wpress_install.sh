f714ce1ae0a697a4c258c66c24317c618c6b35e3
EC2 Default User <ec2-user@ip-172-16-100-16.eu-west-2.compute.internal>
2020-08-11 00:57:33 +0000

    added wordpress install script

---
+++ wpress_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install -y httpd
+
+cd /var/www/html/
+sudo wget https://wordpress.org/latest.zip
+sudo unzip latest.zip 
+sudo mv wordpress/* .
+sudo rm -f latest.zip 
+sudo rmdir wordpress/
+
